bcc24aa4b0fb0638f47915ac673037b417d8371c
reamd <dajunjn@163.com>
2016-05-28 22:36:55 +0800

    增加gulp构建工具压缩js和css

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+/**
+ * Created by reamd on 5/28/2016.
+ */
+var gulp = require('gulp'),
+    mincss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    rename = require('gulp-rename'),
+    del = require('del');
+
+    gulp.task('scripts', function() {
+        return gulp.src('src/*.js')
+            .pipe(rename({suffix: '.min'}))
+            .pipe(uglify())
+            .pipe(gulp.dest('dist/'))
+            .pipe(notify({ message: 'Scripts task complete' }));
+    });
+
+    gulp.task('styles', function() {
+        return gulp.src('src/*.css')
+            .pipe(rename({suffix: '.min'}))
+            .pipe(mincss())
+            .pipe(gulp.dest('dist/'))
+            .pipe(notify({ message: 'Styles task complete' }));
+    });
+
+    gulp.task('clean', function(cb) {
+        del(['dist/'], cb)
+    });
+
+    gulp.task('default', function() {
+        gulp.run
+        ('clean', 'styles', 'scripts');
+    });
